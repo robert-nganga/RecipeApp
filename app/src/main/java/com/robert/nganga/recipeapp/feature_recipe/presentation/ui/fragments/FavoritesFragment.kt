264b1f775d9cc9c0660c482d53a05a0703c6088a
robert-nganga <robertnganga220@gmail.com>
2023-02-23 16:25:42 +0300

    added navigation with bottom navigation view

---
+++ app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/fragments/FavoritesFragment.kt
@@ -0,0 +1,29 @@
+package com.robert.nganga.recipeapp.feature_recipe.presentation.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.robert.nganga.recipeapp.R
+import com.robert.nganga.recipeapp.databinding.FragmentFavoriteBinding
+
+class FavoritesFragment: Fragment(R.layout.fragment_favorite) {
+
+    private var _binding : FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/fragments/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.robert.nganga.recipeapp.feature_recipe.presentation.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.robert.nganga.recipeapp.R
+import com.robert.nganga.recipeapp.databinding.FragmentHomeBinding
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+
+
+    private var _binding : FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
