78a3b4f0eded928de8f01356fb1b025e00035cd7
robert-nganga <robertnganga220@gmail.com>
2023-02-27 21:25:07 +0300

    recipe header image

--- app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/fragments/RecipeFragment.kt
+++ app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/fragments/RecipeFragment.kt
@@ -1,4 +1,36 @@
 package com.robert.nganga.recipeapp.feature_recipe.presentation.ui.fragments
 
-class RecipeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.robert.nganga.recipeapp.R
+import com.robert.nganga.recipeapp.databinding.FragmentRecipeBinding
+import com.robert.nganga.recipeapp.feature_recipe.presentation.RecipeViewModel
+import com.robert.nganga.recipeapp.feature_recipe.presentation.ui.MainActivity
+
+class RecipeFragment: Fragment(R.layout.fragment_recipe){
+    private lateinit var viewModel: RecipeViewModel
+    private var _binding : FragmentRecipeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as MainActivity).viewModel
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
