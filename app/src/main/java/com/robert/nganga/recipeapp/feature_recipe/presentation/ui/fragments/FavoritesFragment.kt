e682fc973cfb37b191efaf2f96528a79b4fa05e5
robert-nganga <robertnganga220@gmail.com>
2023-03-14 20:48:11 +0300

    favorite item click listener

--- app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/fragments/FavoritesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -41,6 +42,13 @@ class FavoritesFragment: Fragment(R.layout.fragment_favorite) {
 
         binding.floatingActionButton.setOnClickListener {
             showConfirmDialog()
+        }
+
+        favoriteAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putInt("id", it.id)
+            }
+            findNavController().navigate(R.id.action_favoritesFragment_to_recipeFragment, bundle)
         }
 
         viewModel.favorites.observe(viewLifecycleOwner) { favorites ->
