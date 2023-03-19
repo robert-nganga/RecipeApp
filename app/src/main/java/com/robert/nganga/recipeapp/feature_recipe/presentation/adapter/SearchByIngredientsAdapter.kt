3385eb864045f47e5f1bdabbe61d9b180baf17fa
robert-nganga <robertnganga220@gmail.com>
2023-03-19 13:17:47 +0300

    search by ingredients adapter

---
+++ app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/adapter/SearchByIngredientsAdapter.kt
@@ -0,0 +1,54 @@
+package com.robert.nganga.recipeapp.feature_recipe.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.robert.nganga.recipeapp.databinding.RecipeByIngredientItemBinding
+import com.robert.nganga.recipeapp.feature_recipe.domain.model.RecipeByIngredients
+
+class SearchByIngredientsAdapter: RecyclerView.Adapter<SearchByIngredientsAdapter.SearchByIngredientsViewHolder>() {
+
+    val differ = AsyncListDiffer(this, DIFF_CALLBACK)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchByIngredientsViewHolder {
+        val binding = RecipeByIngredientItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SearchByIngredientsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchByIngredientsViewHolder, position: Int) {
+        val recipeByIngredients = differ.currentList[position]
+        holder.setData(recipeByIngredients)
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    inner class SearchByIngredientsViewHolder(private val binding: RecipeByIngredientItemBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun setData(recipeByIngredients: RecipeByIngredients){
+            binding.apply {
+                recipeByIngredients.apply {
+                    tvRecipeName.text = title
+                    Glide.with(itemView).load(image).into(imgSearchByIngredientRecipe)
+                }
+            }
+        }
+
+    }
+
+    companion object{
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<RecipeByIngredients>(){
+            override fun areItemsTheSame(oldItem: RecipeByIngredients, newItem: RecipeByIngredients): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: RecipeByIngredients, newItem: RecipeByIngredients): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
