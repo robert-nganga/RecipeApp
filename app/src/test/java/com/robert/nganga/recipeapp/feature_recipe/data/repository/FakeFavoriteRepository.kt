d75763f03e064f92c784da40bfce296fe9098d90
robert-nganga <robertnganga220@gmail.com>
2023-04-03 10:23:33 +0300

    fake favoriteRepository

--- app/src/test/java/com/robert/nganga/recipeapp/feature_recipe/data/repository/FakeFavoriteRepository.kt
+++ app/src/test/java/com/robert/nganga/recipeapp/feature_recipe/data/repository/FakeFavoriteRepository.kt
@@ -2,14 +2,13 @@ package com.robert.nganga.recipeapp.feature_recipe.data.repository
 
 import com.robert.nganga.recipeapp.core.util.Resource
 import com.robert.nganga.recipeapp.feature_recipe.domain.model.Recipe
-import com.robert.nganga.recipeapp.feature_recipe.domain.repository.RecipeRepository
+import com.robert.nganga.recipeapp.feature_recipe.domain.repository.FavoriteRepository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-class FakeRecipeRepository: RecipeRepository {
+class FakeFavoriteRepository: FavoriteRepository{
 
     private val recipes = mutableListOf<Recipe>()
-
-    private var shouldReturnNetworkError = false
 
     init {
         recipes.add(
@@ -59,37 +58,65 @@ class FakeRecipeRepository: RecipeRepository {
                 dairyFree = true,
                 aggregateLikes = 2,
                 sourceName = "sourceName2",
-                readyInMinutes = 2,
                 servings = 2,
                 sourceUrl = "sourceUrl2",
                 isFavorite = true,
                 cookingMinutes = 2,
+                readyInMinutes = 2,
                 tag = "myTag",
                 timeStamp = "timeStamp2"
             )
         )
+        recipes.add(
+            Recipe(
+                id = 3,
+                title = "Recipe 3",
+                image = "image3",
+                imageType = "imageType3",
+                summary = "summary3",
+                cuisines = emptyList(),
+                dishTypes = emptyList(),
+                diets = emptyList(),
+                instructions = "instructions3",
+                analyzedInstructions = emptyList(),
+                extendedIngredients = emptyList(),
+                vegetarian = true,
+                vegan = true,
+                glutenFree = true,
+                dairyFree = true,
+                aggregateLikes = 3,
+                sourceName = "sourceName3",
+                servings = 3,
+                sourceUrl = "sourceUrl3",
+                isFavorite = true,
+                cookingMinutes = 3,
+                readyInMinutes = 3,
+                tag = "myTag",
+                timeStamp = "timeStamp3"
+            )
+        )
     }
 
-    override fun getRandomRecipes(tag: String)= flow {
-        if (shouldReturnNetworkError) {
-            emit(Resource.error("Network error has occurred"))
-        } else {
-            val list = recipes.filter { it.tag == tag }
-            emit(Resource.success(list))
+    override fun getFavoriteRecipes() = flow {
+        emit(Resource.success(recipes.filter { it.isFavorite }))
+    }
+
+    override fun getFavoriteRecipeById(id: Int) = flow {
+        emit(Resource.success(recipes.first { it.id == id }))
+    }
+
+    override suspend fun addFavorite(favorite: Recipe) {
+        recipes[recipes.indexOf(recipes.first { it.id == favorite.id })] = favorite
+    }
+
+    override suspend fun removeFavorite(favorite: Recipe) {
+        recipes[recipes.indexOf(recipes.first { it.id == favorite.id })] = favorite
+    }
+
+    override suspend fun removeAllFavorite(favorites: List<Recipe>) {
+        favorites.forEach { favorite ->
+            recipes[recipes.indexOf(recipes.first { it.id == favorite.id })] = favorite
         }
     }
 
-    override fun getRecipeById(id: Int) = flow {
-        if (shouldReturnNetworkError) {
-            emit(Resource.error("Network error has occurred"))
-        } else {
-            emit(Resource.success(recipes.first { it.id == id }))
-        }
-    }
-    override suspend fun updateRecipe(recipe: Recipe) {
-        val recipeIndex = recipes.indexOfFirst { it.id == recipe.id }
-        if (recipeIndex > -1) {
-            recipes[recipeIndex] = recipe
-        }
-    }
 }
---
+++ app/src/test/java/com/robert/nganga/recipeapp/feature_recipe/presentation/viewmodel/FavoriteViewModelTest.kt
@@ -0,0 +1,28 @@
+package com.robert.nganga.recipeapp.feature_recipe.presentation.viewmodel
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class FavoriteViewModelTest {
+
+    @Test
+    fun getFavorites() {
+    }
+
+    @Test
+    fun insertFavoriteRecipe() {
+    }
+
+    @Test
+    fun deleteFavoriteRecipe() {
+    }
+
+    @Test
+    fun deleteAllFavoriteRecipes() {
+    }
+
+    @Test
+    fun toggleFavorite() {
+    }
+}
