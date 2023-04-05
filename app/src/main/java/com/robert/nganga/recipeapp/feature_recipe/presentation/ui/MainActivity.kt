60a3e36de6c93841d49542f03280e40466f75576
robert-nganga <robertnganga220@gmail.com>
2023-04-05 12:54:55 +0300

    bottomNavView invisible in searchFragment

--- app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/robert/nganga/recipeapp/feature_recipe/presentation/ui/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         navController.addOnDestinationChangedListener{ _, destination, _ ->
             when(destination.id){
                 R.id.recipeFragment -> {binding.bottomNavView.visibility = View.GONE}
+                R.id.searchFragment -> {binding.bottomNavView.visibility = View.GONE}
                 else -> {binding.bottomNavView.visibility = View.VISIBLE}
             }
 
